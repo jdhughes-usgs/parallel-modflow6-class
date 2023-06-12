fc4ea7d1b464ffb5ddfa8f20883d1387dcba40e9
Joseph Hughes <jdhughes@usgs.gov>
2023-06-12 18:20:52 -0500

    update notebooks

--- notebooks/defaults.py
+++ notebooks/defaults.py
@@ -2,6 +2,7 @@ import os
 
 import matplotlib as mpl
 import numpy as np
+import pathlib as pl
 import shapely
 from matplotlib import colors
 
@@ -281,3 +282,84 @@ def write_petscdb(ws, ncpus):
             petsc_file.write("-dvclose 1.0e-6\n")
             petsc_file.write("-options_left no\n")
     return
+
+def _build_nproc(nrow_blocks, ncol_blocks):
+    if nrow_blocks * ncol_blocks == 0:
+        nproc = max(nrow_blocks, ncol_blocks)
+    else:
+        nproc = nrow_blocks * ncol_blocks
+    return nproc
+    
+def _build_workspace(nrow_blocks, ncol_blocks):
+    nproc = _build_nproc(nrow_blocks, ncol_blocks)
+    if nrow_blocks * ncol_blocks == 0:
+        parallel_ws = parallel_ws = pl.Path(f"../examples/basin_metis_{nproc}p")
+    else:
+        parallel_ws = pl.Path(f"../examples/basin_{nrow_blocks}x{ncol_blocks}_{nproc}p")   
+    return parallel_ws
+
+def set_parallel_data(nrow_blocks, ncol_blocks):
+    # trap potential errors
+    if nrow_blocks == 0 and ncol_blocks == 0:
+        raise ValueError("nrow_blocks or ncol_blocks must be greater than 0")
+    
+    # derive a few variables from parallel settings above
+    nproc = _build_nproc(nrow_blocks, ncol_blocks)
+    parallel_ws = _build_workspace(nrow_blocks, ncol_blocks)
+    if "metis" in str(parallel_ws):
+        use_metis = True
+    else:
+        use_metis = False
+    return use_metis, nproc, parallel_ws
+
+def get_parallel_data(nrow_blocks, ncol_blocks):
+    parallel_ws = _build_workspace(nrow_blocks, ncol_blocks)
+    if not parallel_ws.is_dir():
+        raise FileNotFoundError(f"{str(parallel_ws)} does not exist")
+    return parallel_ws
+
+def simple_mapping(nrow_blocks, ncol_blocks, modelgrid):
+    """
+    Create a simple mapping array for a structured grid
+
+    
+    """
+    if modelgrid.grid_type != "structured":
+        raise ValueError(f"modelgrid must be 'structured' not {gwf.modelgrid.grid_type}")
+    nproc = nrow_blocks * ncol_blocks
+    nrow, ncol = modelgrid.nrow, modelgrid.ncol
+    row_inc, col_inc = int(nrow / nrow_blocks), int(ncol / ncol_blocks)
+
+    # create a list of row boundaries
+    icnt = 0
+    row_blocks = [icnt]
+    for i in range(nrow_blocks):
+        icnt += row_inc
+        row_blocks.append(icnt)
+    if row_blocks[-1] < nrow:
+        row_blocks[-1] = nrow
+
+    # create a list of column boundaries
+    icnt = 0
+    col_blocks = [icnt]
+    for i in range(ncol_blocks):
+        icnt += col_inc
+        col_blocks.append(icnt)
+    if col_blocks[-1] < ncol:
+        col_blocks[-1] = ncol
+
+    # create masking array - zero-based model number
+    mask = np.zeros((nrow, ncol), dtype=int)
+    ival = 0
+    model_row_col_offset = {}
+    for idx in range(len(row_blocks) - 1):
+        for jdx in range(len(col_blocks) - 1):
+            mask[
+                row_blocks[idx] : row_blocks[idx + 1],
+                col_blocks[jdx] : col_blocks[jdx + 1],
+            ] = ival
+            model_row_col_offset[ival - 1] = (row_blocks[idx], col_blocks[jdx])
+            # increment model number
+            ival += 1  
+
+    return mask
